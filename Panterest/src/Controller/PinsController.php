c74b004b52cbaf0df2c7f4b890173260432c3f3c
OthmaneIhiri <ihiri027@gmail.com>
2021-02-06 17:17:02 +0000

    Page d'affichage et navigation entre les pages
    Lier la page d'acceuil et la page d'affichage

--- Panterest/src/Controller/PinsController.php
+++ Panterest/src/Controller/PinsController.php
@@ -3,19 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PinRepository;
+use App\Entity\Pin;
 
 class PinsController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * 
      */
-    public function index(PinRepository $pinRepository)
+    public function index(PinRepository $pinRepository): Response
     {
 
         $pins = $pinRepository->findAll();
         
         return $this->render('pins/index.html.twig',compact('pins'));
     }
+
+    /**
+     * 
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     * 
+     */
+
+     public function show(Pin $pin): Response
+     {
+
+        return $this->render('pins/show.html.twig', compact('pin'));
+
+     }
+
 }
