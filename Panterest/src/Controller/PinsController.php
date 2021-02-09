c4f4d39c7d9326bc31e162e41a062097a0d87cdf
OthmaneIhiri <ihiri027@gmail.com>
2021-02-09 17:47:09 +0000

    Ajouter la fonct de suppression
    Ajouter la fonct de suppression et l'afficher dans la page show

--- Panterest/src/Controller/PinsController.php
+++ Panterest/src/Controller/PinsController.php
@@ -103,7 +103,19 @@ class PinsController extends AbstractController
 
     }
 
-     
-     
+     /**
+     * 
+     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods={"DELETE"})
+     * 
+     */
+
+    public function delete(Pin $pin, EntityManagerInterface $em): Response
+    {
+            $em->remove($pin);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+
+    }
 
 }
